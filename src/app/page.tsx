bb4d682f695f6b9c3d2187940996449288d28e0b
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-02-22 11:45:47 -0300

    feat: add fonts config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
 export default function Home() {
   return (
     <main className="">
       <h1>Hola Teslo Shop</h1>
+      <h1 className={titleFont.className}>Hola Teslo Shop</h1>
     </main>
   );
 }
