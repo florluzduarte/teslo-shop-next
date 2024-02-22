6ca17e89c844aa26136ebdf3e49f9536550ef474
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-02-22 12:39:36 -0300

    feat: initial routes and product images

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,9 +2,9 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
       <h1>Hola Teslo Shop</h1>
       <h1 className={titleFont.className}>Hola Teslo Shop</h1>
-    </main>
+    </div>
   );
 }
