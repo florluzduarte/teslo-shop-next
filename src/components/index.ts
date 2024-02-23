2d5d8c0375c9ad667537085056c56311609ff6e4
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-02-23 14:33:04 -0300

    feat: add Title and ProductGrid components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
+//Products
+export { ProductGrid } from "./products/product-grid/ProductGrid";
+
 // UI
 export { PageNotFound } from "./ui/not-found/PageNotFound";
+export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/TopMenu";
