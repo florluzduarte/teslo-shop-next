b0e5c2b7780278835969b3257e7361eabec22a86
Florencia Luz Duarte <florluzduarte@gmail.com>
2024-03-13 12:07:32 -0300

    feat: seed data and seed database configured

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -2,11 +2,52 @@ import { initialData } from "./seed";
 import prisma from "../lib/prisma";
 
 async function main() {
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ])
+  //Limpio todos los campos de la base de datos antes de empezar a popularla
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  // Data que viene del seed
+  const { categories, products } = initialData;
+
+  // Inserto categorias
+  const initialCategories = categories.map((category) => ({
+    name: category
+  }));
+
+  await prisma.category.createMany({
+    data: initialCategories
+  });
+
+  // Relación entre Productos y Categorias
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((key, category) => {
+    key[category.name.toLowerCase()] = category.id
+    return key;
+  }, {} as Record<string, string>) //<string=category.name>=<string=category.id>
+
+  // Inserción de productos
+  products.forEach(async (product) => {
+    const { images, type, ...correctProduct } = product;
+    const productDB = await prisma.product.create({
+      data: {
+        ...correctProduct,
+        categoryId: categoriesMap[type]
+      }
+    })
+
+    // Inserción de imágenes
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: productDB.id
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData
+    })
+  })
+
   console.log("Seed ejecutado correctamente");
 }
 
